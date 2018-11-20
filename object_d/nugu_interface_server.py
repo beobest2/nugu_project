5cf0aafed8b12b2a1b08e1a5e261d278ce43c7e6
itholic <haejoon309@naver.com>
2018-11-20 16:41:43 +0900

    [add] 시간 옵션 처리

--- object_d/nugu_interface_server.py
+++ object_d/nugu_interface_server.py
@@ -124,18 +124,47 @@ class Live():
                 json_data = None
 
             """ FIXME :  disappear_time에 watched가 마지막으로 존재했던 시간을 할당
-            1. watched는 json_data['parameters']['watched']['value'] 에 담겨있음
-            2. 이는 현재 화면에서 watched가 인식되지 않을 경우에만 처리해주면 됨
+            1. watched는 json_data['action']['parameters']['watched']['value'] 에 담겨있음
+            2. disappear_time은 현재 화면에서 watched가 인식되지 않을 경우에만 할당해주면 됨
             3. 현재 화면에서 인식 가능한 경우에는 disappear_time 에 0 을 주면 됨
-            4. 128 ~ 130 번째 줄 코드를 수정하면 됨. 현재는 테스트용으로 1과 0중 하나가 들어가게 해놓았음 """
+            4. 옵션으로 'hour_', 'min_', 'now_' 값이 올 수도 있음
+                - json_data['action']['parameters']['hour_'] (min_, now_) 가 존재하면 옵션이 있는 경우임
+                - hour_, min_ ,now_ 중 아무것도 존재하지 않으면 옵션을 주지 않은 것임
+                - hour_ 의 경우 앞에 'D.'이 붙어서 옴 (ex. D.1, D.2)
+                - 옵션이 있는 경우 현재 시간에서 해당 시간을 뺀 시점에 'watched'가 있었는지 확인하면됨
+                - 옵션이 없는 경우는 현재 화면을 기준으로 판단하면 됨
+                - 즉, 옵션이 없는 경우와 옵션이 '지금'인 경우는 동일하게 처리
+            5. 현재는 테스트용으로 임의의 값을 설정해놓았음 """
+
+            # 아래는 테스트용임
             import random
-            disappear_time = random.randrange(0,2)
-            #disappear_time = 1
+            disappear_time = random.choice([0, "13시 13분"])  # 0인 경우는 인식 가능, 후자는 인식 불가시 사라진 시간
+
+            """ FIXME : watched가 'UNKNOWN' 으로 전달되었을때 처리
+            - 처음 보는 객체가 발견됐다면, unknown에 해당 객체명 할당 
+            - disappear_time은 1을 할당  
+            - 처음 보는 객체가 없다면 disappear_time에 -1을 할당 """
+
+            unknown = 0  # unknown default값. 뭐가 오든 상관없음
+            watched = json_data['action']['parameters']['watched']['value']
+            if watched == "UNKNOWN":
+                disappear_time = random.choice([1, -1])  # 특이객체 있을경우 1, 없을 경우 -1
+                if disappear_time == 1:  # 특이객체 있을경우 처리, 현재는 랜덤값을 넣어놓았음
+                    unknown = random.choice(["귀신", "도꺠비", "팀장님"])
+
+            """ disappear_time 경우의수
+            -1 : 이상한사람에 대한 질문시, 이상한사람이 없을경우
+            1 : 이상한사람에 대한 질문시, 이상한사람이 있을경우
+            0 : 미리 등록해놓은 사람에 대한 질문시, 자리에 있을 경우
+            실제값 : 미리 등록해놓은 사람에 대한 질문시, 사라진 시간 ("xx시 xx분" 형태로 할당)
+            """
+
             rtn = {
                     "version": "2.0",
                     "resultCode": "OK",
                     "output": {
                         "result": True,
+                        "unknown": unknown,
                         "disappear_time": disappear_time
                     }
                 }
@@ -160,12 +189,7 @@ class Live():
             except:
                 json_data = None
 
-            """ FIXME : disappear_time에 앞에서 처리한 값을 그대로 넣어준다 
-            1. disappear_time은 json_data['action']['parameters']['disappear_time']['value'] 에 담겨있음
-            2. 167 ~ 168 줄을 수정하면 됨. 현재는 테스트 용으로 임의의 시간을 넣어놨음
-            """
-            import random
-            disappear_time = "{}시 {}분".format(random.randrange(0,24), random.randrange(0,60))
+            disappear_time = json_data['action']['parameters']['disappear_time']['value']  # "xx시 xx분" 형태로 할당
             rtn = {
                     "version": "2.0",
                     "resultCode": "OK",
@@ -177,6 +201,7 @@ class Live():
 
             print("[not_exist] json.dumps(rtn) : {}".format(json.dumps(rtn)))
             return json.dumps(rtn)
+
 
         @app.route("/Watcher/answer.capture", methods=["POST"])
         def watcher_answer_capture():
@@ -193,9 +218,15 @@ class Live():
                 json_data = None
 
             """ FIXME : 사진을 이메일로 전송한다.
-            이메일 주소는 임의로 등록한 값이며, 서버단에서 설정파일등을 통해 처리
-            메일을 보낸후 resultCode를 OK로 보내주면 된다.
-            메일 전송이 실패한 경우는 아직 처리하지 않는다.
+            1. 이메일 주소는 임의로 등록한 값이며, 서버단에서 설정파일등을 통해 처리
+            2. 옵션으로 'hour', 'min', 'now' 값이 올 수도 있음
+                - json_data['action']['parameters']['hour'] (min, now) 가 존재하면 옵션이 있는 경우임
+                - hour, min ,now 중 아무것도 존재하지 않으면 옵션을 주지 않은 것임
+                - 옵션이 있는 경우 현재로부터 해당 시간만큼 뺀 시점의 사진을 메일로 보내주면 됨
+                - 옵션이 없는 경우는 현재 사진을 찍어서 메일로 보내주면 됨
+                - 즉, 옵션이 없는 경우와 옵션이 '지금'인 경우는 동일하게 처리
+            3. 메일을 보낸후 resultCode를 OK로 보내주면 된다.
+            4. 메일 전송이 실패한 경우는 아직 처리하지 않는다.
             """
             rtn = {
                 "resultCode": "OK"
